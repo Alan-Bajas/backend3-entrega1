3f5be2c7f1f34e672faf4cfc4f1419150520fcab
Alan Bajas <alanbajas@gmail.com>
2026-02-15 11:41:12 -0300

    feat: add mongoose models for users and pets

---
+++ src/dao/models/pet.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const petSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    type: { type: String, required: true },
+    age: { type: Number, required: true }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("pets", petSchema);
---
+++ src/dao/models/user.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+    pets: { type: Array, default: [] }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("users", userSchema);
